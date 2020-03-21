3359b85c0b95afd7237885c4b05faf4ffccd683b
hoang6k <daohoang.hust@gmail.com>
2020-03-22 04:30:53 +0700

    first version oop

--- genetic_oop.py
+++ genetic_oop.py
@@ -7,48 +7,82 @@ np.random.seed(0)
 
 
 class Chromosome:
+    _pct_change = None
+    _z_score = 1.0
+
     def __init__(self, weight):
         if weight is None:
             weight = []
         self._weight = weight
-        self._fitness = None
+        self._fitness = self.calculate_fitness()
     
-    def calculate_fitness(self, pct_change, z_score):
-        print(pct_change)
-        print(self._weight)
-        value_ptf = pct_change * self._weight
+    def calculate_fitness(self):
+        value_ptf = Chromosome._pct_change * self._weight * 1e6
         value_ptf['Value of Portfolio'] = value_ptf.sum(axis=1)
         ptf_percentage = value_ptf['Value of Portfolio']
         ptf_percentage = ptf_percentage.sort_values(axis=0, ascending=True)
-        _VaR =  np.percentile(ptf_percentage, z_score)
+        _VaR =  np.percentile(ptf_percentage, Chromosome._z_score)
         self._fitness = -_VaR
         return self._fitness
 
 
 
 class Population:
-    def __init__(self, pct_change, z_score: float = 1.0, first_population: list = None):
+    def __init__(self, first_population: list = None):
         if first_population is None:
             first_population = []
-        self._pct_change = pct_change
-        self._z_score = z_score
         self._population_size = len(first_population)
         self._generations = [first_population]
-        _fitness = np.asarray([chromo.calculate_fitness(self._pct_change, self._z_score) for chromo in first_population])
-        self._generations_fitness = [_fitness]
+        _fitness = [chromo._fitness for chromo in first_population]
+        self._best_fitness = np.min(np.asarray(_fitness))
+        self._all_best_fitness = [self._best_fitness]
         self._generations_solution = first_population[np.argmin(_fitness)]
-        self._print = False
+        self.verbose = False
     
+    def mutation(self, children):
+        new_children = []
+        chosen_indexes = np.random.choice(len(children), size=self._mutation_size, replace=False)
+        for i in range(len(children)):
+            if i not in chosen_indexes:
+                new_children.append(copy.deepcopy(children[i]))
+                continue
+            chromosome = children[i]
+            if self.verbose > 1:
+                print('\t\tStarting mutation {}th child'.format(len(new_children) + 1))
+            _mutation = bool(np.random.rand(1) <= self._mutation_probability)
+            if _mutation:
+                mutation_genes_indexes = np.random.choice(
+                                            np.arange(len(chromosome._weight)), 
+                                            size=np.random.randint(len(chromosome._weight)), 
+                                            replace=False)
+                sorted_indexes = np.sort(mutation_genes_indexes)
+                new_weight = np.array(chromosome._weight)
+                new_weight[sorted_indexes] = chromosome._weight[mutation_genes_indexes]
+                new_children.append(Chromosome(new_weight))
+        return new_children
 
-    def crossover(self, father, mother, alpha=0.5):
-        child_1 = (1 - alpha) * father + alpha * mother
-        child_2 = alpha * father + (1 - alpha) * mother
-        return child_1, child_2
-
+    def crossover(self, parents, alpha=None):
+        children = []
+        for i in range(int(self._offspring_number / 2)):
+            if self.verbose > 1:
+                print('\t\t{}_th 2 childs'.format(i + 1))
+            _crossover = bool(np.random.rand(1) <= self._crossover_probability)
+            if _crossover:
+                index = np.random.randint(len(parents))
+                father = parents.pop(index)
+                index = np.random.randint(len(parents))
+                mother = parents.pop(index)
+                if alpha is None:
+                    alpha = father._fitness / (father._fitness + mother._fitness)
+                child_1 = Chromosome((1 - alpha) * father._weight + alpha * mother._weight)
+                child_2 = Chromosome(alpha * father._weight + (1 - alpha) * mother._weight)
+                children.append(child_1)
+                children.append(child_2)
+        return children
 
     def roulette_wheel_selection(self, generation, k):
         # for i in range(int(self._offspring_number / 2)):
-        #     if self._print:
+        #     if self.verbose:
         #         print('\t\t{}_th 2 childs'.format(i + 1))
         #     _crossover = bool(np.random.rand(1) <= self._crossover_probability)
         #     if _crossover:
@@ -63,40 +97,13 @@ class Population:
         pass
 
     def tournament_selection(self, generation, k, alpha):
-        start_time = time()
         fitness = np.asarray([chromo._fitness for chromo in generation])
         parents = []
-        fitness_parents = []
-        children = []
-
-        # select mating pool
         for i in range(self._offspring_number):
             chosen_indexes = np.random.choice(self._population_size, size=k, replace=False)
             best_index = chosen_indexes[np.argmin(fitness[chosen_indexes])]
-            parents.append(generation[best_index])
-            fitness_parents.append(fitness_parents[best_index])
-        
-        # crossover mating pool
-        for i in range(int(self._offspring_number / 2)):
-            _crossover = bool(np.random.rand(1) <= self._crossover_probability)
-            if _crossover:
-                index = np.random.randint(len(parents))
-                father = parents.pop(index)
-                father_fitness = fitness_parents.pop(index)
-                index = np.random.randint(len(parents))
-                mother = parents.pop(index)
-                mother_fitness = fitness_parents.pop(index)
-                alpha = father_fitness / (father_fitness + mother_fitness)
-                child_1, child_2 = self.crossover(father, mother, alpha)
-                children.append(child_1)
-                children.append(child_2)
-        print('Time of cross-over: {} seconds'.format(time() - start_time))
-        children_fitness = np.max(np.asarray([chromo.calculate_fitness() for chromo in children]))
-        print('\tCROSS-OVER fitness: {}'.format(children_fitness))
-
-
-
-        pass
+            parents.append(copy.deepcopy(generation[best_index]))
+        return parents
 
     def rank_selection(self, generation, k):
         pass
@@ -108,16 +115,13 @@ class Population:
         pass
 
     def generate_next_population(self):
-        print('\nIteration {}'.format(len(self._generations_fitness)))
-        current_generation = self._generations[-1]
-        current_generation_fitness = self._generations_fitness[-1]
-        fitness = np.asarray([chromo._fitness for chromo in current_generation])
+        if self.verbose > 0:
+            print('\nIteration {}'.format(len(self._all_best_fitness)))
+        generation = self._generations[-1]
+        np.random.shuffle(generation)
+        generation_fitness = np.asarray([chromo._fitness for chromo in generation])
 
-        # cross-over phase
-        start_time = time()
-        if self._print:
-            print('----CROSS-OVER PHASE')
-
+        # selection phase
         selection_switcher = {
             'roulette_wheel': self.roulette_wheel_selection,
             'tournament': self.tournament_selection,
@@ -125,81 +129,117 @@ class Population:
             'boltzmann': self.boltzmann_selection,
             'elitism': self.elitism_selection
         }
-        
-        children = selection_switcher.get(self._selection_method['type'], \
-                                        lambda: 'Invalid selection method')(\
-                                            current_generation,\
-                                            self._selection_method['k'],\
-                                            self._selection_method['alpha'])
-        if isinstance(children, str):
-            pass
-        
+        parents = selection_switcher.get(
+                    self._selection_method['type'], 
+                    lambda: 'Invalid selection method')(
+                    generation,
+                    self._selection_method['k'],
+                    self._selection_method['alpha'])
 
-        pass
+        # cross-over phase
+        if self.verbose > 0:
+            print('----CROSS-OVER PHASE')
+            start_time = time()
+        children = self.crossover(parents)
+        best_fitness = np.min([chromo._fitness for chromo in children])
+        if self.verbose > 0:
+            if self.verbose > 1:
+                print('Time of cross-over: {} seconds'.format(time() - start_time))
+            print('\tCROSS-OVER best fitness: {}'.format(best_fitness))
+
+        # mutation phase
+        if self.verbose > 0:
+            print('****MUTATION PHASE')
+            start_time = time()
+        new_children = self.mutation(children)
+        best_fitness = np.min(np.asarray([chromo._fitness for chromo in new_children]))
+        if self.verbose > 0:
+            if self.verbose > 1:
+                print('Time of mutation: {} seconds'.format(time() - start_time))
+            print('\tMUTATION best fitness: {}'.format(best_fitness))
+
+        # replace worst chromosomes
+        sorted_indexes = np.argsort(generation_fitness)
+        worst_indexes = sorted_indexes[-self._chromosomes_replace:]
+        worst_indexes.sort()
+        worst_indexes = np.flip(worst_indexes)
+        for idx in worst_indexes:
+            generation.pop(idx)
+        new_generation = generation + new_children
+        new_generation_fitness = np.asarray([chromo._fitness for chromo in new_generation])
+        self._generations.append(new_generation)
+        self._all_best_fitness.append(np.min(new_generation_fitness))
+        self._generations_solution = new_generation[np.argmin(new_generation_fitness)]
+        return self._all_best_fitness[-1]
+
+    def print(self):
+        print('Population size: ' + str(self._population_size))
+        print('Offspring number: ' + str(self._offspring_number))
+        print('Selection type: ' + self._selection_method['type'].capitalize())
+        print('Crossover probability: ' + str(self._crossover_probability))
+        print('Mutation probability: ' + str(self._mutation_probability))
+        print('Mutation size: ' + str(self._mutation_size))
+        print('Max generations number: ' + str(self._generations_number))
+        print('Stop criterion depth: ' + str(self._stop_criterion_depth), end='\n\n')
 
 
-    def generate_populations(self, config: dict, _print=False):
-        self._offspring_number = config['offspring_number']
+    def generate_populations(self, config: dict, verbose=False):
+        self._offspring_number = int(self._population_size * config['offspring_ratio'])
         self._crossover_probability = config['crossover_probability']
         self._selection_method = config['selection_method']
         self._mutation_probability = config['mutation_probability']
         self._mutation_size = config['mutation_size']
-        self._chromosomes_replace = config['chromosomes_replace']
+        self._chromosomes_replace = self._offspring_number
         self._generations_number = config['generations_number']
         self._stop_criterion_depth = config['stop_criterion_depth']
-        self._print = _print
+        self.verbose = verbose
+        self.print()
 
-        current_generation_fitness = self._generations_fitness[-1]
-        current_solution = self._generations_solution[-1]
-        print('Initial fitness: {}'.format(current_generation_fitness))
-        
+        print('Initial fitness: {}'.format(self._best_fitness))        
         depth = 0
-        best_fitness = current_generation_fitness
         for epoch in range(self._generations_number):
-            new_generation_fitness, new_generation_solution = self.generate_next_population()
-            print('Generation {}: fitness {}'.format(epoch + 1, new_generation_fitness))
-            if new_generation_fitness <= best_fitness:
+            new_best_fitness = self.generate_next_population()
+            print('Generation {}: fitness {}'.format(epoch + 1, new_best_fitness))
+            if new_best_fitness >= self._best_fitness:
                 depth += 1
-                print('\tFitness not increase for {} generations'.format(depth))
+                if self.verbose > 0:
+                    print('\tFitness not improved for {} generations'.format(depth))
                 if depth > self._stop_criterion_depth:
-                    print('**********STOP CRITERION DEPTH REACHED**********')
+                    if self.verbose > 0:
+                        print('**********STOP CRITERION DEPTH REACHED**********')
                     break
             else:
-                print('\tFitness increased')
+                if self.verbose > 0:
+                    print('\tFitness improved')
                 depth = 0
-                best_fitness = new_generation_fitness
+                self._best_fitness = new_best_fitness
 
 
     @staticmethod
     def population_initialization(df, z_score: float = 1.0, population_size: int = 100, genes_number: int = None):
         df.drop(['DTYYYYMMDD'], axis=1, inplace=True)
-        pct_change = df.pct_change()
+        Chromosome._pct_change = df.pct_change()
+        Chromosome._z_score = z_score
         new_population = np.random.dirichlet(np.ones(genes_number), size=population_size)
-        return Population(pct_change, z_score, [Chromosome(chromo) for chromo in new_population])
-
-
+        return Population([Chromosome(chromo) for chromo in new_population])
 
 
 if __name__ == '__main__':
-    config = {'population_size': 200, 'offspring_number': 50,
+    config = {'population_size': 100, 'offspring_ratio': 0.5,
                 'crossover_probability': 1.0,
                 'selection_method': {'type': 'tournament', 'k': 10, 'alpha': 0.4},
-                'mutation_probability': 0.66, 'mutation_size': 10,
-                'chromosomes_replace': 50,
+                'mutation_probability': 1.0, 'mutation_size': 10,
                 'generations_number': 500, 'stop_criterion_depth': 50}
                
-    config['offspring_number'] = int(config['population_size'] / 2)
-    config['chromosomes_replace'] = int(config['population_size'] / 2)
 
     path = 'data/data_concat.csv'
     df = pd.read_csv(path)
     genes_number = len(df.columns) - 1
     z_score = 1.0
-    print(genes_number)
 
     population = Population.population_initialization(df, z_score, population_size=config['population_size'], \
                                                         genes_number=genes_number)
-
+    population.generate_populations(config=config, verbose=1)
 
     pass
 
