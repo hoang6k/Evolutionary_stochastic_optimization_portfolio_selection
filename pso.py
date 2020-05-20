be12b4959b8686279ddeac46e7af239c00990cc8
CMC TextMining <daohoang.hust@gmail.com>
2020-05-20 20:01:34 +0700

    update

--- pso.py
+++ pso.py
@@ -9,276 +9,28 @@ np.random.seed(0)
 
 
 class Particle(Chromosome):
-    _pct_change = None
-    _z_score = None
-    _lambda = None
-    _method = None
-    _annual_returns = None
-    _annual_cov_matrix = None
-
     def __init__(self, weight):
-        if weight is None:
-            weight = []
-        self._weight = weight
-        if Chromosome._method == 'VaR':
-            self._fitness = self.calculate_VaR_fitness()
-        elif Chromosome._method == 'VaRp':
-            self._fitness = self.calculate_VaRp_fitness()
-        elif Chromosome._method == 'markovitz':
-            self._fitness = self.calculate_markovitz_fitness()
-        elif Chromosome._method == 'markovitz_sqrt':
-            self._fitness = self.calculate_markovitz_fitness_sqrt()
-        elif Chromosome._method == 'sharp_coef':
-            self._fitness = self.calculate_sharp_coef_fitness()
-        elif Chromosome._method == 'sharp_coef_sqrt':
-            self._fitness = self.calculate_sharp_coef_fitness_sqrt()
-        else:
-            exit()
-
-    def calculate_VaR_fitness(self):
-        value_ptf = Chromosome._pct_change * self._weight * 1e6
-        value_ptf['Value of Portfolio'] = value_ptf.sum(axis=1)
-        ptf_percentage = value_ptf['Value of Portfolio']
-        ptf_percentage = ptf_percentage.sort_values(axis=0, ascending=True)
-        _VaR =  np.percentile(ptf_percentage, Chromosome._z_score)
-        self._fitness = -_VaR
-        return self._fitness
-
-    def calculate_VaRp_fitness(self):
-        port_variance = np.dot(self._weight, np.dot(Chromosome._annual_cov_matrix, self._weight.T))
-        port_standard_devitation = np.sqrt(port_variance)
-        port_returns_expected = np.sum(self._weight * Chromosome._annual_returns)
-        self._fitness = (- port_returns_expected + 2.33 * port_standard_devitation) * 1e6
-        if self._fitness < 0:
-            print('unexpected fitness < 0')
-            exit()
-        return self._fitness
-
-    def calculate_markovitz_fitness(self):
-        _lambda = Chromosome._lambda
-        port_variance = np.dot(self._weight, np.dot(Chromosome._annual_cov_matrix, self._weight.T))
-        port_standard_devitation = np.sqrt(port_variance)
-        port_returns_expected = np.sum(self._weight * Chromosome._annual_returns)
-        self._fitness = (_lambda * port_standard_devitation - (1 - _lambda) * port_returns_expected) * 1e6
-        return self._fitness
-
-    def calculate_markovitz_fitness_sqrt(self):
-        _lambda = Chromosome._lambda
-        port_variance = np.dot(self._weight, np.dot(Chromosome._annual_cov_matrix, self._weight.T))
-        port_standard_devitation = np.sqrt(port_variance)
-        port_returns_expected = np.sum(self._weight * Chromosome._annual_returns)
-        self._fitness = (_lambda * np.sqrt(port_standard_devitation) - (1 - _lambda) * port_returns_expected) * 1e6
-        return self._fitness
-
-    def calculate_sharp_coef_fitness(self):
-        port_variance = np.dot(self._weight, np.dot(Chromosome._annual_cov_matrix, self._weight.T))
-        port_standard_devitation = np.sqrt(port_variance)
-        port_returns_expected = np.sum(self._weight * Chromosome._annual_returns)
-        self._fitness = - port_returns_expected / port_standard_devitation * 1e6
-        return self._fitness
-
-    def calculate_sharp_coef_fitness_sqrt(self):
-        port_variance = np.dot(self._weight, np.dot(Chromosome._annual_cov_matrix, self._weight.T))
-        port_standard_devitation = np.sqrt(port_variance)
-        port_returns_expected = np.sum(self._weight * Chromosome._annual_returns)
-        self._fitness = - port_returns_expected / np.sqrt(port_standard_devitation) * 1e6
-        return self._fitness
+        super().__init__(weight)
+        self._best_position = self._weight
 
     @staticmethod
-    def calculate_sd_e(df, z_score, _lambda, optimize,
-                        inertia_factor=inertia_factor,
-                        self_conf_factor=self_conf_factor,
-                        swarm_conf_factor=swarm_conf_factor):
+    def calculate_sd_e(df, z_score, _lambda, optimize, inertia_factor, self_conf_factor, swarm_conf_factor):
         df.drop(['DTYYYYMMDD'], axis=1, inplace=True)
-        Chromosome._method = optimize
-        Chromosome._z_score = z_score
-        Chromosome._lambda = _lambda
-        Chromosome._pct_change = df.pct_change()
-        Chromosome._annual_returns = Chromosome._pct_change.mean()
-        Chromosome._annual_cov_matrix = Chromosome._pct_change.cov()
+        Particle._method = optimize
+        Particle._z_score = z_score
+        Particle._lambda = _lambda
+        Particle._pct_change = df.pct_change()
+        Particle._annual_returns = Particle._pct_change.mean()
+        Particle._annual_cov_matrix = Particle._pct_change.cov()
+        Particle._inertia_factor = inertia_factor
+        Particle._self_conf_factor = self_conf_factor
+        Particle._swarm_conf_factor = swarm_conf_factor
 
 
 class Swarm(Population):
     def __init__(self, first_population: list = None):
-        if first_population is None:
-            first_population = []
-        self._population_size = len(first_population)
-        self._generations = [first_population]
-        _fitness = [chromo._fitness for chromo in first_population]
-        self._best_fitness = np.min(np.asarray(_fitness))
-        self._all_best_fitness = [self._best_fitness]
-        self._generations_solution = [first_population[np.argmin(_fitness)]]
-        self._best_solution = self._generations_solution[-1]
-        self.verbose = False
+        super().__init__(first_population=first_population)
     
-    def mutation(self, children):
-        new_children = []
-        chosen_indexes = np.random.choice(len(children), size=self._mutation_size, replace=False)
-        for i in range(len(children)):
-            if i not in chosen_indexes:
-                new_children.append(copy.deepcopy(children[i]))
-                continue
-            chromosome = children[i]
-            if self.verbose > 1:
-                print('\t\tStarting mutation {}th child'.format(len(new_children) + 1))
-            _mutation = bool(np.random.rand(1) <= self._mutation_probability)
-            if _mutation:
-                mutation_genes_indexes = np.random.choice(
-                                            np.arange(len(chromosome._weight)), 
-                                            size=np.random.randint(len(chromosome._weight)), 
-                                            replace=False)
-                sorted_indexes = np.sort(mutation_genes_indexes)
-                new_weight = np.array(chromosome._weight)
-                new_weight[sorted_indexes] = chromosome._weight[mutation_genes_indexes]
-                new_children.append(Chromosome(new_weight))
-        return new_children
-
-    def crossover_all(self, parents, alpha=None):
-        pass
-
-    def uniform_crossover(self, parents, alpha=None):
-        genes_number = len(parents[0]._weight)
-        children = []
-        for i in range(int(self._offspring_number / 2)):
-            if self.verbose > 1:
-                print('\t\t{}_th 2 childs'.format(i + 1))
-            _crossover = bool(np.random.rand(1) <= self._crossover_probability)
-            if _crossover:
-                index = np.random.randint(len(parents))
-                father = parents.pop(index)._weight
-                index = np.random.randint(len(parents))
-                mother = parents.pop(index)._weight
-                crossover_genes_indexes = np.random.choice(
-                                            np.arange(genes_number), 
-                                            size=np.random.randint(genes_number), 
-                                            replace=False)
-                sorted_indexes = np.sort(crossover_genes_indexes)
-                _cs_genes_father = father[sorted_indexes]
-                _cs_genes_mother = mother[sorted_indexes]
-                cs_genes_father = _cs_genes_father * np.sum(_cs_genes_mother) / np.sum(_cs_genes_father)
-                cs_genes_mother = _cs_genes_mother *  np.sum(_cs_genes_father) / np.sum(_cs_genes_mother)
-                father[sorted_indexes] = cs_genes_mother
-                mother[sorted_indexes] = cs_genes_father
-                weight_1 = father
-                weight_2 = mother
-                overweight_1 = np.where(weight_1 > 0.4)
-                if len(overweight_1[0]) == 1:
-                    weight_1 += (weight_1[overweight_1[0][0]] - 0.4) / (len(weight_1) - 1)
-                    weight_1[overweight_1[0][0]] = 0.4
-                elif len(overweight_1[0]) == 2:
-                    weight_1 += (weight_1[overweight_1[0][0]] + weight_1[overweight_1[0][1]] - 0.8) / (len(weight_1) - 2)
-                    weight_1[overweight_1[0][0]] = 0.4
-                    weight_1[overweight_1[0][1]] = 0.4
-                overweight_2 = np.where(weight_2 > 0.4)
-                if len(overweight_2[0]) == 1:
-                    weight_2 += (weight_2[overweight_2[0][0]] - 0.4) / (len(weight_2) - 1)
-                    weight_2[overweight_2[0][0]] = 0.4
-                elif len(overweight_2[0]) == 2:
-                    weight_2 += (weight_2[overweight_2[0][0]] + weight_2[overweight_2[0][1]] - 0.8) / (len(weight_2) - 2)
-                    weight_2[overweight_2[0][0]] = 0.4
-                    weight_2[overweight_2[0][1]] = 0.4
-                children.append(Chromosome(weight_1))
-                children.append(Chromosome(weight_2))
-        return children
-
-    def 2points_crossover(self, parents, alpha=None):
-        genes_number = len(parents[0]._weight)
-        children = []
-        for i in range(int(self._offspring_number / 2)):
-            if self.verbose > 1:
-                print('\t\t{}_th 2 childs'.format(i + 1))
-            _crossover = bool(np.random.rand(1) <= self._crossover_probability)
-            if _crossover:
-                index = np.random.randint(len(parents))
-                father = parents.pop(index)._weight
-                index = np.random.randint(len(parents))
-                mother = parents.pop(index)._weight
-                two_points = np.random.choice(np.arange(genes_number), size=2, replace=False)
-                two_points.sort()
-                _cs_genes_father = father[two_points[0]:two_points[1] + 1]
-                _cs_genes_mother = mother[two_points[0]:two_points[1] + 1]
-                cs_genes_father = _cs_genes_father * np.sum(_cs_genes_mother) / np.sum(_cs_genes_father)
-                cs_genes_mother = _cs_genes_mother *  np.sum(_cs_genes_father) / np.sum(_cs_genes_mother)
-                try:
-                    weight_1 = np.concatenate((father[:two_points[0]], cs_genes_mother, father[two_points[1] + 1:]))
-                    weight_2 = np.concatenate((mother[:two_points[0]], cs_genes_father, mother[two_points[1] + 1:]))
-                except IndexError:
-                    weight_1 = np.concatenate((father[:two_points[0]], cs_genes_mother))
-                    weight_2 = np.concatenate((mother[:two_points[0]], cs_genes_father))
-                overweight_1 = np.where(weight_1 > 0.4)
-                if len(overweight_1[0]) == 1:
-                    weight_1 += (weight_1[overweight_1[0][0]] - 0.4) / (len(weight_1) - 1)
-                    weight_1[overweight_1[0][0]] = 0.4
-                elif len(overweight_1[0]) == 2:
-                    weight_1 += (weight_1[overweight_1[0][0]] + weight_1[overweight_1[0][1]] - 0.8) / (len(weight_1) - 2)
-                    weight_1[overweight_1[0][0]] = 0.4
-                    weight_1[overweight_1[0][1]] = 0.4
-                overweight_2 = np.where(weight_2 > 0.4)
-                if len(overweight_2[0]) == 1:
-                    weight_2 += (weight_2[overweight_2[0][0]] - 0.4) / (len(weight_2) - 1)
-                    weight_2[overweight_2[0][0]] = 0.4
-                elif len(overweight_2[0]) == 2:
-                    weight_2 += (weight_2[overweight_2[0][0]] + weight_2[overweight_2[0][1]] - 0.8) / (len(weight_2) - 2)
-                    weight_2[overweight_2[0][0]] = 0.4
-                    weight_2[overweight_2[0][1]] = 0.4
-                children.append(Chromosome(weight_1))
-                children.append(Chromosome(weight_2))
-        return children
-
-    def 1point_crossover(self, parents, alpha=None):
-        children = []
-        for i in range(int(self._offspring_number / 2)):
-            if self.verbose > 1:
-                print('\t\t{}_th 2 childs'.format(i + 1))
-            _crossover = bool(np.random.rand(1) <= self._crossover_probability)
-            if _crossover:
-                index = np.random.randint(len(parents))
-                father = parents.pop(index)
-                index = np.random.randint(len(parents))
-                mother = parents.pop(index)
-                if alpha is None:
-                    alpha = father._fitness / (father._fitness + mother._fitness)
-                child_1 = Chromosome((1 - alpha) * father._weight + alpha * mother._weight)
-                child_2 = Chromosome(alpha * father._weight + (1 - alpha) * mother._weight)
-                children.append(child_1)
-                children.append(child_2)
-        return children
-
-    def roulette_wheel_selection(self, generation, k=5):
-        fitness = np.asarray([chromo._fitness for chromo in generation])
-        if Chromosome._method in ['VaR', 'VaRp']:
-            fitness = 1 - fitness / np.sum(fitness)
-        else:
-            # min-max scaling
-            _min = np.min(fitness)
-            _max = np.max(fitness)
-            fitness = (_max - fitness + 0.1) / (_max - _min + 0.2)
-        fitness /= np.sum(fitness)
-        parents = []
-        for _ in range(self._offspring_number):
-            chosen_indexes = np.random.choice(np.arange(len(fitness)), size=k, replace=False, p=fitness)
-            best_index = chosen_indexes[np.argmax(fitness[chosen_indexes])]
-            parents.append(copy.deepcopy(generation[best_index]))
-        return parents
-
-    def tournament_selection(self, generation, k):
-        fitness = np.asarray([chromo._fitness for chromo in generation])
-        parents = []
-        for _ in range(self._offspring_number):
-            chosen_indexes = np.random.choice(self._population_size, size=k, replace=False)
-            best_index = chosen_indexes[np.argmin(fitness[chosen_indexes])]
-            parents.append(copy.deepcopy(generation[best_index]))
-        return parents
-
-    def rank_selection(self, generation, k):
-        pass
-
-    def boltzmann_selection(self, generation, k):
-        pass
-    
-    def elitism_selection(self, generation, k):
-        pass
-
     def generate_next_population(self):
         if self.verbose > 0:
             print('\nIteration {}'.format(len(self._all_best_fitness)))
@@ -306,9 +58,9 @@ class Swarm(Population):
             print('----CROSS-OVER PHASE')
             start_time = time()
         crossover_switcher = {
-            '1point': self.1point_crossover,
-            '2points': self.2points_crossover,
-            'uniform': self.uniform_crossover
+            '1point': self.crossover_1point,
+            '2points': self.crossover_2points,
+            'uniform': self.crossover_uniform
         }
         children = crossover_switcher.get(
                     self._crossover_method['type'],
@@ -360,7 +112,6 @@ class Swarm(Population):
         print('Mutation size: ' + str(self._mutation_size))
         print('Max generations number: ' + str(self._generations_number))
         print('Stop criterion depth: ' + str(self._stop_criterion_depth), end='\n\n')
-
 
     def generate_populations(self, config: dict, verbose=False):
         self._offspring_number = int(self._population_size * config['offspring_ratio'])
@@ -418,7 +169,7 @@ class Swarm(Population):
                                 self_conf_factor=self_conf_factor,
                                 swarm_conf_factor=swarm_conf_factor)
         new_population = np.random.dirichlet(np.ones(genes_number), size=population_size)
-        return Population([Chromosome(chromo) for chromo in new_population])
+        return Swarm([Particle(chromo) for chromo in new_population])
 
 
 if __name__ == '__main__':
